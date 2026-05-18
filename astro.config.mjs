49b1ce1e54eff9d655dd91658826d26aa98b5c98
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2026-05-17 17:04:29 -0700

    add sitemap + robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dcrader-template-photographers.vercel.app',
+  integrations: [sitemap()],
   output: 'static',
   compressHTML: true,
   prefetch: true,
